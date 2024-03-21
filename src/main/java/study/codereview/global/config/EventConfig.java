5775c253af43b11fcbf81ed1d4f7e8a5e07de986
devholic22 <hyunjoon.tech@gmail.com>
2024-03-22 02:27:57 +0900

    refactor: EventConfig 패키지 분리
    - EventConfig 패키지 이동

--- src/main/java/study/codereview/global/config/EventConfig.java
+++ src/main/java/study/codereview/global/config/EventConfig.java
@@ -1,10 +1,11 @@
-package study.codereview.global.event;
+package study.codereview.global.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import study.codereview.global.event.Events;
 
 @RequiredArgsConstructor
 @Configuration
